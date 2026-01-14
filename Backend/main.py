09cf51a4b501731d0a8bb14bbc89342b394459f8
IngAmaury <hortamaury@gmail.com>
2026-01-14 16:57:42 -0600

    fix: The asynchronous function not executing has been fixed.

--- Backend/main.py
+++ Backend/main.py
@@ -1,12 +1,20 @@
-from fastapi import FastAPI, HTTPException
+from db import engine
+from db import get_db
+from fastapi import FastAPI, HTTPException, Depends
+from models import Base
+from models import Simulation
 from schemas import SimulateRequest
 from services import calculate_french_amortization, risk_audit
+from sqlalchemy.orm import Session
 import asyncio
+import json
+
+Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
 @app.post("/simulate")
-def simulate(payload: SimulateRequest):
+async def simulate(payload: SimulateRequest, db: Session = Depends(get_db),):
     try:
         mp, tp, ti, schedule = calculate_french_amortization(
             amount=payload.amount,
@@ -14,15 +22,31 @@ def simulate(payload: SimulateRequest):
             term_months=payload.months,
         )
         
-        simulation_id = 1
+        sim = Simulation(
+            amount=payload.amount,
+            rate=payload.rate,
+            months=payload.months,
+            monthly_payment=mp,
+            total_paid=tp,
+            total_interest=ti,
+            schedule_json=json.dumps(schedule, ensure_ascii=False),
+            audit_status="PENDING",
+        )
 
-        asyncio.create_task(risk_audit(simulation_id, payload.amount, payload.rate, payload.months))
-        return {"ok": True, "data": {
-            "monthly_payment": mp,
-            "total_paid": tp,
-            "total_interest": ti,
-            "schedule": schedule},
-            }
+        db.add(sim)
+        db.commit()
+        db.refresh(sim)
+
+        asyncio.create_task(risk_audit(sim.id, payload.amount, payload.rate, payload.months))
+        return {"ok": True, 
+                "simulation_id": sim.id,
+                "audit_status": "PENDING",
+                "data": {
+                    "monthly_payment": mp,
+                    "total_paid": tp,
+                    "total_interest": ti,
+                    "schedule": schedule},
+                }
 
     except ValueError as e:
         raise HTTPException(status_code=422, detail=str(e))
